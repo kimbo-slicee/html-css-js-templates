ac702fa749deaf6e1c38632a48bdae4144a09bb5
mohammed el aouri <mohammedelaouri@gmail.com>
2025-05-25 19:33:26 +0100

    `Feat:Code refactoring`

--- 03-Real-Estate-Web-App/data/about.js
+++ 03-Real-Estate-Web-App/data/about.js
@@ -1,8 +1,4 @@
 const ABOUT={
-    section:"About Us",
-    title:"Experience the Difference in Real Estate Expertise with Us",
-    description:"With a commitment to excellence, we deliver unmatched expertise in real estate. Our team provides" +
-        " personalized solutions, ensuring every property journey is seamless, informed, and tailored to your unique needs.",
     services:[
         {icon:"devices-search",title:"Browse Properties",description:"Explore a diverse selection of properties tailored to your needs."},
         {icon:"calendar-week",title:" Schedule a Visit",description: " Arrange viewings at your convenience to find the perfect fit."},
--- 03-Real-Estate-Web-App/script.js
+++ 03-Real-Estate-Web-App/script.js
@@ -1,103 +1,77 @@
-import createElement from "./utils/createElement.js";
+import { initTheme } from "./utils/theme.js";
 import SERVICES from "./data/services.js";
-import ABOUT from "./data/about.js";
 import PROPERTIES from "./data/properties.js";
-const cardsContainer = document.querySelector(".cards");
-const about=document.querySelector(".about-us .about-content");
-const propertiesSection=document.querySelector("#properties")
+const navItems=document.querySelectorAll("ul a");
+const cards=document.querySelector(".cards");
+const propertiesContainer=document.querySelector(".properties-container");
+const showMorBtn=document.querySelector(".show-more-btn button")
+let cardsNumberDisplay=3
 
-// create Services Cards
-function createServiceCard({icon,title,service}) {
-    const card = createElement("div", "service-card");
-
-    // Header
-    const cardHeader = createElement("div", "card-header");
-    const headerIcon = createElement("i", `ti ti-${icon}`);
-    const h1 = createElement("h1", "", title);
-    cardHeader.append(headerIcon, h1);
-
-    // Body
-    const cardBody = createElement("div", "card-body");
-    const p = createElement("p", "", service);
-    cardBody.append(p);
-
-    // Footer
-    const cardFooter = createElement("div", "card-footer");
-    const button = createElement("button", "", "Let Talk");
-    const footerIcon = createElement("i", "ti ti-arrow-narrow-right");
-    cardFooter.append(button, footerIcon);
-
-    card.append(cardHeader, cardBody, cardFooter);
-    return card;
-}
-// Render all cards
-SERVICES.forEach(service => {
-    const card = createServiceCard(service);
-    cardsContainer.appendChild(card);
-});
-// Create About Sections Content
-function createAboutUs({section,title,description,services}) {
-    const sectionName=createElement("p","section-name",section);
-    const about_title=createElement("h1","about-title",title);
-    const aboutDescription=createElement("p","about-description",description);
-    const whatWeOffer=services.map(({icon,title,description})=>{
-        const serviceContainer=createElement("div","container")
-        const wrapper=createElement("div","wrapper")
-        const iconElement=createElement("i",`ti ti-${icon}`);
-        const h1=createElement("h1","service-title",title);
-        const p=createElement("p","description",description)
-        const button=createElement("button","discover-now-btn","Discover Now")
-        const arrowIcon = createElement("i", "ti ti-chevron-right");
-        button.appendChild(arrowIcon);
-        serviceContainer.append(iconElement);
-        wrapper.append(h1,p,button)
-        serviceContainer.append(wrapper);
-        return serviceContainer
-    })
-    about.append(sectionName,about_title,aboutDescription,...whatWeOffer)
-}
-createAboutUs(ABOUT);
-// Create Properties Section
-const cards=(properties)=>{
-    properties.forEach(({title,bedrooms,bathrooms,description,square_feet,price,agent_name,location,image,agent_image})=>{
-        const div=createElement("div","card");
-        const propertyImage=createElement("img","property-image")
-        propertyImage.setAttribute("src",image);
-        // create card Header
-        const cardHeader=createElement("div","card-header");
-        const propertyName=createElement("h3","property-name",title);
-        const propertyPrice=createElement("h2","property-price",price);
-        cardHeader.append(propertyName,propertyPrice);
-        // create card body
-        const cardBody=createElement("div","card-body");
-        const propertyDescription=createElement("p","property-description",description);
-        const propertyDetails=createElement("div","property-details");
-        const bedroomsNumber=createElement("span","rooms-number",bedrooms)
-        const bedroomIcon=createElement("i","ti ti-bed");
-        bedroomsNumber.insertAdjacentElement('afterbegin',bedroomIcon);
-        const bathRoomsNumber=createElement("span","rooms-number",bathrooms)
-        const bathroomIcon=createElement("i","ti ti-bath");
-        bathRoomsNumber.insertAdjacentElement('afterbegin',bathroomIcon);
-        bedroomsNumber.insertAdjacentElement('afterbegin',bedroomsNumber);
-        const space=createElement("span","rooms-number",square_feet)
-        const rulerIcon=createElement("i","ti ti-ruler-2");
-        space.insertAdjacentElement('afterbegin',rulerIcon);
-        //card footer
-        const cardFooter=createElement("div","card-footer");
-        const agentImage=createElement("img","agent-image");
-        agentImage.src=agent_image;
-        
+/*show More Properties */
+const showMore=()=>{
+    showMorBtn.addEventListener("click",()=>{
+     cardsNumberDisplay+=3;
+        properties(PROPERTIES,cardsNumberDisplay);
     })
 }
+/*Check active section*/
+navItems.forEach((item)=>{
+        item.addEventListener("click",()=>{
+            navItems.forEach((item)=>item.classList.remove("active"))
+            item.classList.add("active")
+        });
 
-function creatProperties ({title,subTitle,description,properties}){
-    const div=createElement("div","wrapper")
-    const h4=createElement("h4","properties-title",title)
-    const h1=createElement("h1","sub-properties-title",subTitle)
-    const p=createElement("p","p-properties-description",description);
-    // create properties header
-    div.append(h4,h1,p);
-    propertiesSection.append(div)
-    cards(properties)
+})
+/*show home heading*/
+const services=(services)=>{
+    cards.innerHTML=services.map(({icon,title,service})=>{
+    return `
+    <div class="card">
+            <div class="card-header">
+                <span class="material-symbols-outlined">${icon}</span>
+                <h1>${title}</h1>
+            </div>
+            <div class="card-body">
+            <article>${service}</article>
+            </div>
+            <div class="card-footer">
+            <button>Let Talk</button>
+            <span class="material-symbols-outlined">arrow_right_alt</span>
+            </div>
+    </div>`
+    }).join('')
 }
-creatProperties(PROPERTIES)
+const properties=({properties},cardsNumberDisplay)=>{
+    propertiesContainer.innerHTML= properties.map((property)=>{
+        return ` <div class="card">
+                <div class="card-image">
+                    <img src=${property.image} alt="propritie image">
+                </div>
+                <div class="card-body">
+                    <div class="card-heading">
+                        <h3>${property.title}</h3>
+                        <h1>${property.price}</h1>
+                    </div>
+                    <article class="property-description">${property.description}</article>
+                    <div class="property-details">
+                    <p>Bed ${property.bedrooms} <span class="material-symbols-outlined">hotel</span></p>
+                    <p>Bath ${property.bathrooms} <span class="material-symbols-outlined">bathtub</span></p>
+                    <p>Feet ${property.square_feet} <span class="material-symbols-outlined">square_foot</span></p>
+                    </div>
+                </div>
+                <div class="card-footer">
+                        <img src=${property.agent_image} alt="agent-image">
+                        <div class="footer-info">
+                            <p>${property.agent_name}</p>
+                            <span>${property.location}</span>
+                        </div>
+                </div>
+        </div>
+    `
+    }).slice(0,cardsNumberDisplay).join(' ');
+
+}
+showMore();
+properties(PROPERTIES,cardsNumberDisplay)
+services(SERVICES)
+initTheme();
